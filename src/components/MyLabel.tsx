336d29e3b19c5d25a288ab21ef184122bb1c7fd3
juampymdd <juampymdd@gmail.com>
2022-01-27 11:19:05 -0300

    first commit my sb

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,43 @@
+import './mylabel.css'
+import { AllCaps } from '../stories/components/MyLabel.stories';
+
+export interface MyLabelProps {
+    /**
+     * Este es el mensaje a mostrar dentro de la etiqueta 
+     */
+    label: string;
+    /**
+     *  Este es el tamano de la etiqueta
+     */
+    size: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+     *  indica si quiere todo capitalizado
+     */
+    allCaps?: boolean;
+    /**
+     *  Este es color de la etiqueta
+     */
+    color?: 'primary' | 'secondary' | 'tertiary';
+    /**
+     *  Color personalizado de la fuente
+     */
+    fontColor?: string;
+}
+
+export const MyLabel = ( {
+    allCaps = false,
+    color   = "primary",
+    label   = "No Label",
+    size    = "normal",
+    fontColor
+}: MyLabelProps ) => {
+    return (
+        <span className={ `label ${ size } text-${ color }` } style={{color:fontColor}} >
+                { allCaps ? label.toUpperCase() : label }
+        </span>
+    );
+};
+
+
+// CustomFontColor
+// fontColor
